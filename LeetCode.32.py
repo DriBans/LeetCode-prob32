05b77830759f48e83f53bbbe2cfec1135120468f
DriBans <drishti.bansal@outlook.com>
2022-06-28 18:05:25 -0400

    Solution to problem 32 (Level: Hard) --Python3

--- LeetCode.32.py
+++ LeetCode.32.py
@@ -1,11 +1,17 @@
+# We do not have to use class as we have a standard operators in this case. However, LeetCode requires class function since they are not supported by OOP (Object-Oriented Programming).
+# I will write a separate code without using class later. Let's solve this problem for now. The first two lines of code below are given in leetcode so we can continue from there.
+
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        """ Stack is a data structure that starts with the ending of the bracket."""
-        stack, ans = [-1], 0
+        """ Stack is a function that starts from the end of the list (s: str)."""
+        stack = [-1]
+        a = 0
         for i in range(len(s)):
-            if s[i] == '(': stack.append()
-            elif len(stack) == 1: stack[0] = 1
+            if s[i] == '(':
+                stack.append()
+            elif len(stack) == 1:
+                stack[0] = 1
             else:
                 stack.pop()
-                ans = max(ans, i - stack[-1])
+                a = max(a, i - stack[-1])
         return
